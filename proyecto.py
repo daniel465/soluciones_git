7e0624d3bce7ead4b70fab9762dba4c5c8038e68
daniel <130743@unsaac.edu.pe>
2020-05-17 02:29:23 -0500

    Implementacion de cant de dias en el año
    -Se implementa un diseño de impresion para agregar este proceso
    -Calculamos la cantidad de dias que podriamos vivir cada mes y sumarlo
    hasta terminar el año.(ver la cantidad de dias que podriamos vivir)

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,7 @@
 #Determina la cantidad de dias que has vivido
 #importamos date
 from datetime import date
+import calendar
 ahora=date.today()
 #ingresamos la fecha nacimiento
 print("Ingresa la fecha de nacimiento ")
@@ -11,3 +12,17 @@ fecha_nace=date(agno,mes,dia)
 #hallamos la cantidad de dias
 todaydias=(ahora-fecha_nace).days
 print("La cantidad de dias vividos son : ",todaydias)
+#La cantidad de dias que viviras en los siguientes meses
+print('='*70)
+print('Impresion de cantidad de dias que viviremos')
+print('='*70)
+acumular=0
+fecha_aux=ahora
+for i in range(ahora.month,13):
+    fecha_aux=fecha_aux.replace(month=i, day=calendar.monthrange(fecha_aux.year,i)[1])
+    cant_dias=(fecha_aux-ahora).days
+    acumular+=cant_dias
+    print("Para el año %d/%d, la cant de dias q vivirias es :%d, cant dias mes : %d/ %d"%(fecha_aux.year,i,todaydias+acumular,cant_dias,acumular))
+    ahora=ahora.replace(month=i,day=calendar.monthrange(ahora.year,i)[1])
+print('='*70)
+print('Total de dias que podriasmos vivir en este año seria : ',(acumular+todaydias))
