625db5086091dc3035b556aede5b0c683085d3a2
daniel <130743@unsaac.edu.pe>
2020-05-16 22:31:26 -0500

    Imprimiendo la fecha de nacimineto y la actual
    -Imprimimos el dia en que nacio
    -Imprimimosla fecha actual con referencia

--- proyecto.py
+++ proyecto.py
@@ -8,6 +8,12 @@ agno=int(input("Digite el año :"))
 mes=int(input("Digite el mes :"))
 dia=int(input("Digite el dia :"))
 fecha_nace=date(agno,mes,dia)
+#Fecha en que nacimiento fue :
+print('La fecha de nacimiento fue :')
+print('\t',fecha_nace.strftime('%A %d, %B %Y'))
+#Fecha actual de referencia es :
+print('Fecha actual de referencia :')
+print('\t',ahora.strftime('%A %d, %B %Y'))
 #hallamos la cantidad de dias
 todaydias=(ahora-fecha_nace).days
 print("La cantidad de dias vividos son : ",todaydias)
